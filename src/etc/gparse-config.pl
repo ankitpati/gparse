d516e7daa34059a3d0a17bfe70c26e3d73b0f3de
Ankit Pati <contact@ankitpati.in>
2019-04-09 12:00:02 +0530

    more robust config, correct syntax highlighting for config

---
+++ src/etc/gparse-config.pl
@@ -0,0 +1,27 @@
+my $pid_file = $ENV{GPARSE_PID_FILE} // '/tmp/gparse.pid';
+my $tls_cert = $ENV{GPARSE_TLS_CERT} // '/etc/ssl/certs/gparse.pem';
+my $tls_key = $ENV{GPARSE_TLS_KEY} // '/etc/ssl/private/gparse.pem';
+
+# Some PSGI environments export `$PORT` and expect the
+# app to bind only an HTTP (not HTTPS) listener to it.
+my $http_port = $ENV{PORT} // 80;
+
+{
+    hypnotoad => {
+        listen => [
+            # Do not bind HTTPS listener if `$PORT` is exported.
+            $ENV{PORT} ? () : "https://*:443?cert=$tls_cert&key=$tls_key",
+
+            "http://*:$http_port",
+        ],
+        pid_file => $pid_file,
+    },
+
+    minify => {
+        do_javascript => 'shrink',
+        do_stylesheet => 'minify',
+        html5 => 1,
+        remove_comments => 1,
+        remove_newlines => 1,
+    },
+}
