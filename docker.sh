57f1b2f2c714649dbd9e35961cbdf1b6591e0610
Ankit Pati <contact@ankitpati.in>
2019-09-16 15:07:12 +0530

    nutshell can now handle array arguments on sudo systems

--- docker.sh
+++ docker.sh
@@ -3,10 +3,8 @@
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
-test "$#" -ne 0 && args=(bash -lc "$*")
-
 docker build --tag='gparsedev' . && \
 docker run --mount type=bind,src="$(pwd)",dst='/opt/gparse' \
     --publish 3000:3000 \
     --publish 8080:8080 \
-    -it gparsedev "${args[@]}"
+    -it gparsedev "$@"
