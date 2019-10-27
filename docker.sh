24485b753460c7940d5674c1e7b7bee6e32428fc
Ankit Pati <contact@ankitpati.in>
2019-10-27 21:25:00 +0000

    make-like timestamp checking of Dockerfile & resultant image

--- docker.sh
+++ docker.sh
@@ -5,6 +5,13 @@ image='gparsedev'
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
-docker image inspect "$image" &>/dev/null || \
+{
+    docker image inspect "$image" &>/dev/null && \
+    file_epoch="$(stat -c'%Y' 'Dockerfile')" && \
+    image_ts="$(docker image inspect "$image" | jq -r '.[].Created')" && \
+    image_epoch="$(date -d"$image_ts" +'%s')" && \
+    test "$file_epoch" -lt "$image_epoch"
+    # `docker build` only if `Dockerfile` is newer than tagged image.
+} || \
 docker build --tag="$image" . && \
-docker run -it "$image" "$@"
+docker run -it "$image" "$@" # `docker run` if nothing above fails.
