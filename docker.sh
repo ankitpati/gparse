a231a581aad1ab562dc16c3f58e81de8f81ecc4c
Ankit Pati <contact@ankitpati.in>
2019-10-27 23:39:05 +0530

    correctly process arguments for bash

--- docker.sh
+++ docker.sh
@@ -5,6 +5,8 @@ image='gparsedev'
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
+test "$#" -ne 0 && args=(-lc "$*")
+
 {
     docker image inspect "$image" &>/dev/null && \
     file_epoch="$(stat -c'%Y' 'Dockerfile')" && \
@@ -14,4 +16,4 @@ cd "$mydir"
     # `docker build` only if `Dockerfile` is newer than tagged image.
 } || \
 docker build --tag="$image" . && \
-docker run -it "$image" "$@" # `docker run` if nothing above fails.
+docker run -it "$image" "${args[@]}" # `docker run` if nothing above fails.
