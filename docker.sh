eb2770931e4daf74101b861d70ef9ba93391fdee
Ankit Pati <contact@ankitpati.in>
2019-10-29 07:23:10 +0530

    docker build based on stat times breaks COPY

--- docker.sh
+++ docker.sh
@@ -7,13 +7,5 @@ cd "$mydir"
 
 test "$#" -ne 0 && args=(-lc "$*")
 
-{
-    docker image inspect "$image" &>/dev/null && \
-    file_epoch="$(stat -c'%Y' 'Dockerfile')" && \
-    image_ts="$(docker image inspect "$image" | jq -r '.[].Created')" && \
-    image_epoch="$(date -d"$image_ts" +'%s')" && \
-    test "$file_epoch" -lt "$image_epoch"
-    # `docker build` only if `Dockerfile` is newer than tagged image.
-} || \
 docker build --tag="$image" . && \
 docker run -it "$image" "${args[@]}" # `docker run` if nothing above fails.
