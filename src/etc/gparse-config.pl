69fdb81c9aa93fcf7471e22a1ec98ac5d767a5db
Ankit Pati <contact@ankitpati.in>
2019-12-19 09:02:24 +0530

    enforce TLS verification

--- src/etc/gparse-config.pl
+++ src/etc/gparse-config.pl
@@ -1,7 +1,11 @@
 my $pid_file = $ENV{GPARSE_PID_FILE} // '/tmp/gparse.pid';
-my $tls_ca = $ENV{GPARSE_TLS_CA} // '/etc/ssl/certs/origin-pull-ca.pem';
-my $tls_cert = $ENV{GPARSE_TLS_CERT} // '/etc/ssl/certs/gparse.pem';
-my $tls_key = $ENV{GPARSE_TLS_KEY} // '/etc/ssl/private/gparse.pem';
+
+my %tls = (
+    ca => $ENV{GPARSE_TLS_CA} // '/etc/ssl/certs/origin-pull-ca.pem',
+    verify => SSL_VERIFY_PEER,
+    cert => $ENV{GPARSE_TLS_CERT} // '/etc/ssl/certs/gparse.pem',
+    key => $ENV{GPARSE_TLS_KEY} // '/etc/ssl/private/gparse.pem',
+);
 
 # Some PSGI environments export `$PORT` and expect the
 # app to bind only an HTTP (not HTTPS) listener to it.
@@ -45,7 +49,8 @@ my @csp_self = qw(
     hypnotoad => {
         listen => [
             # Do not bind HTTPS listener if `$PORT` is exported.
-            $ENV{PORT} ? () : "https://*:443?ca=$tls_ca&cert=$tls_cert&key=$tls_key",
+            $ENV{PORT} ? () : "https://*:443?ca=$tls{ca}&verify=$tls{verify}".
+                                           "&cert=$tls{cert}&key=$tls{key}",
 
             "http://*:$http_port",
         ],
