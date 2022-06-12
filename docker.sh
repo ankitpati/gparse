f4d6f6027ccfbcd66262a33f25fae9ed81de36c0
Ankit Pati <contact@ankitpati.in>
2022-06-12 19:04:31 +0530

    docker.sh: do not bunch args together

--- docker.sh
+++ docker.sh
@@ -5,7 +5,5 @@ image='gparse-dev'
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
-test "$#" -ne 0 && args=(-c "$*")
-
 docker build --tag="$image" . && \
-docker run -it "$image" "${args[@]}" # `docker run` if nothing above fails.
+docker run -it "$image" "$@"
