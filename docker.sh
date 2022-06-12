4f55ca5ffbca94e61b806a321170f0f30a9702bc
Ankit Pati <contact@ankitpati.in>
2022-06-12 18:54:22 +0530

    hyphenise dev container, remove `-l` flag as it’s now in `ENTRYPOINT`

--- docker.sh
+++ docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-image='gparsedev'
+image='gparse-dev'
 
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
-test "$#" -ne 0 && args=(-lc "$*")
+test "$#" -ne 0 && args=(-c "$*")
 
 docker build --tag="$image" . && \
 docker run -it "$image" "${args[@]}" # `docker run` if nothing above fails.
