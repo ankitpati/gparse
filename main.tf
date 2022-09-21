5764f7df9f08fb302814429e065306ef7e0d9179
Ankit Pati <contact@ankitpati.in>
2022-09-22 00:21:29 +0530

    Terraform: upgrade to latest stable releases

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.2.9"
+  required_version = ">= 1.3.0"
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = ">= 2.21.0"
+      version = ">= 2.22.0"
     }
   }
 }
