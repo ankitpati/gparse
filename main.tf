fb0afc394a9d946fbce56d4d091ad278d6380495
Ankit Pati <contact@ankitpati.in>
2022-09-22 17:27:41 +0530

    Terraform: remove hardcoded app name references

--- main.tf
+++ main.tf
@@ -8,21 +8,25 @@ terraform {
   }
 }
 
+locals {
+  app_name = "gparse-dev"
+}
+
 resource "docker_image" "gparse-dev" {
-  name         = "gparse-dev"
+  name         = "${local.app_name}-image"
   keep_locally = true
   triggers = {
     dockerfile_hash = filesha256("Dockerfile")
   }
   build {
-    path = dirname(path.module)
-    tag  = ["gparse-dev"]
+    path = path.module
+    tag  = [local.app_name]
   }
 }
 
 resource "docker_container" "gparse-dev" {
-  image   = docker_image.gparse-dev.image_id
-  name    = "gparse-dev"
+  image   = resource.docker_image.gparse-dev.image_id
+  name    = "${local.app_name}-container"
   command = ["morbo", "src/gparse.pl"]
   ports {
     internal = 3000
